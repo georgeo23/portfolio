05830e02fcc1642f0888eda13fb69852cc82c4ae
georgeo23 <olney.george@yahoo.co.uk>
2020-10-27 17:18:06 +0000

    link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ class App extends React.Component {
       <div className="App">
         <Navbar />
         <Switch>
-          <Route exact path="/projects" component={Home} />
+          <Route exact path="/" component={Home} />
           <Route exact path="/projects" component={Projects} />
           <Route exact path="/about" component={About} />
           <Route exact path="/contact" component={Contact} />
